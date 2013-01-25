8aeed5c75fc731a40434c187269bef71527fe8e1
An Cao Luu Bao <kcj.pop.com@gmail.com>
2013-01-25 23:32:34 +0200

    Predefine a map to pass to Image component.

--- assets/js/components/Map.js
+++ assets/js/components/Map.js
@@ -1,6 +1,9 @@
 Crafty.c('Map', {
+	map : [],
 	init: function() {
 		this.addComponent('2D, Canvas');
+		this._create();
+		console.log(this.map);
 	},
 	/**
 	 * Create a new map
@@ -8,7 +11,24 @@ Crafty.c('Map', {
 	 * @param  {int} tree Number of trees
 	 * @return {array}      A 2-dimentional array representing the map
 	 */
-	_create: function(bush, tree) {
-
+	_create: function(width, height, bush, tree) {
+		// As we are targeting iPad resolution (1024 x 768) with 64-pixel block
+		// Our map will have 1024 / 64 = 28 columns and 768 / 64 = 12 rows
+		// 0 will be used to denoted empty blocks, 1 is for bush and 2 is for tree
+		this.map = [
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+			[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+		];
 	}
 });
